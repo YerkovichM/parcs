758c516249a07db35cf3288e1ce922cb5114d138
Marko Yerkovich <myerkovich@magnite.com>
2021-04-08 13:17:57 +0300

    1

--- src/Master.java
+++ src/Master.java
@@ -5,14 +5,20 @@ public class Master {
         task curtask = new task();
         curtask.addJarFile("DFS.jar");
         AMInfo info = new AMInfo(curtask, null);
-        point p = info.createPoint();
-        channel c = p.createChannel();
-        p.execute("DFS");
-        System.out.println("Initial input: SOME BIG STRING");
-        c.write("SOME BIG STRING");
+        point p1 = info.createPoint();
+        point p2 = info.createPoint();
+        System.out.println("Initial input: SOME BIG STRING 2ND PART OF STRING");
+
+        channel c1 = p1.createChannel();
+        p1.execute("DFS");
+        c1.write("SOME BIG STRING");
+
+        channel c2 = p2.createChannel();
+        p2.execute("DFS");
+        c2.write("2ND PART OF STRING");
 
         System.out.println("Waiting for result...");
-        System.out.println("Result: " + c.readObject());
+        System.out.println("Result: " + c1.readObject() + c2.readObject());
         curtask.end();
     }
 }
