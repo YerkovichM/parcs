f993ccc0b3b65bf6c7781e97cade801c64b75eaa
Marko Yerkovich <myerkovich@magnite.com>
2021-04-08 14:03:03 +0300

    1

--- src/DFS.java
+++ src/DFS.java
@@ -29,6 +29,11 @@ public class DFS implements AM {
                 result.append(ch);
             }
         }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 
