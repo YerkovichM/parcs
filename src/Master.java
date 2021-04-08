6c86cffc735548ef9dbe7db480cddcdd5bc4e98b
Marko Yerkovich <myerkovich@magnite.com>
2021-04-08 13:44:24 +0300

    1

--- src/Master.java
+++ src/Master.java
@@ -1,24 +1,34 @@
-import parcs.*;
+import parcs.AMInfo;
+import parcs.channel;
+import parcs.point;
+import parcs.task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Master {
+    final static String STRING = "Wikis are enabled by wiki software, otherwise known as wiki engines. A wiki engine, being a form of a content management system, differs from other web-based systems such as blog software, in that the content is created without any defined owner or leader, and wikis have little inherent structure, allowing structure to emerge according to the needs of the users.[1] Wiki engines usually allow content to be written using a simplified markup language and sometimes edited with the help of a rich-text editor.[2] There are dozens of different wiki engines in use, both standalone and part of other software, such as bug tracking systems. Some wiki engines are open source, whereas others are proprietary. Some permit control over different functions (levels of access); for example, editing rights may permit changing, adding, or removing material. Others may permit access without enforcing access control. Other rules may be imposed to organize content.";
+
     public static void main(String[] args) {
         task curtask = new task();
         curtask.addJarFile("DFS.jar");
         AMInfo info = new AMInfo(curtask, null);
-        point p1 = info.createPoint();
-        point p2 = info.createPoint();
-        System.out.println("Initial input: SOME BIG STRING 2ND PART OF STRING");
 
-        channel c1 = p1.createChannel();
-        p1.execute("DFS");
-        c1.write("SOME BIG STRING");
-
-        channel c2 = p2.createChannel();
-        p2.execute("DFS");
-        c2.write("2ND PART OF STRING");
-
+        List<channel> chns = new ArrayList<>();
+        for (int i = 0; i < STRING.length(); i += 10) {
+            point point = info.createPoint();
+            channel chn = point.createChannel();
+            chns.add(chn);
+            point.execute("DFS");
+            chn.write(STRING.substring(i, i + 10));
+        }
         System.out.println("Waiting for result...");
-        System.out.println("Result: " + c1.readObject() + c2.readObject());
+        String result = chns.stream()
+                .map(channel::readObject)
+                .map(Object::toString)
+                .collect(Collectors.joining());
+        System.out.println("Result: " + result);
         curtask.end();
     }
 }
